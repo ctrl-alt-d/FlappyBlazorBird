be0520103b97206ab589e3f6e10f5b69de417b25
dani <ctrl.alt.d@gmail.com>
2021-02-21 00:23:34 +0100

    fix makedist project

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ ! -d src/FlappyBlazorBird/FlappyBlazorBird.Client ]; then
+if [ ! -d src/FlappyBlazorBird/FlappyBlazorBird.ClientWasm ]; then
    echo "check dirs"
    exit -1
 fi
 
-dotnet publish -c release src/FlappyBlazorBird/FlappyBlazorBird.Client
+dotnet publish -c release src/FlappyBlazorBird/FlappyBlazorBird.ClientWasm
 rm -ifr ./dist
 mkdir ./dist
-mv ./src/FlappyBlazorBird/FlappyBlazorBird.Client/bin/release/net5.0/publish ./dist
+mv ./src/FlappyBlazorBird/FlappyBlazorBird.ClientWasm/bin/release/net5.0/publish ./dist
+
+( cd dist/publish; scp -r * $USER@flappyblazorbird.ctrl-alt-d.net:/home/flappy/apps/flappyblazorbird )
 
 echo "done"
