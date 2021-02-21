75d96f7b1976f1a91a7f8fc9294335d81cd7cc63
dani <ctrl.alt.d@gmail.com>
2021-02-21 17:50:32 +0100

    net6 branch

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -8,8 +8,8 @@ fi
 dotnet publish -c release src/FlappyBlazorBird/FlappyBlazorBird.ClientWasm
 rm -ifr ./dist
 mkdir ./dist
-mv ./src/FlappyBlazorBird/FlappyBlazorBird.ClientWasm/bin/release/net5.0/publish ./dist
+mv ./src/FlappyBlazorBird/FlappyBlazorBird.ClientWasm/bin/release/net6.0/publish ./dist
 
-( cd dist/publish; scp -r * $USER@flappyblazorbird.ctrl-alt-d.net:/home/flappy/apps/flappyblazorbird )
+( cd dist/publish/wwwroot; scp -r * $SSHUSER@flappyblazorbird.ctrl-alt-d.net:/home/flappy/apps/flappyblazorbird )
 
 echo "done"
